8680fbccb17b036c0bd0e8dd6898520fbdc8f2e2
sloria <sloria1@gmail.com>
2014-08-10 11:16:09 -0400

    It's 2014

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.abspath("_themes"))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'marshmallow'
-copyright = u'2013'
+copyright = u'2014'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
