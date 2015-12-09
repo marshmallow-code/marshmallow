90a0d89279636e3e598c5c76ec7e2326c9825219
Steven Loria <sloria1@gmail.com>
2015-12-09 00:39:37 -0500

    Update changelog and bump version

--- marshmallow/__init__.py
+++ marshmallow/__init__.py
@@ -13,7 +13,7 @@ from marshmallow.decorators import (
 from marshmallow.utils import pprint, missing
 from marshmallow.exceptions import ValidationError
 
-__version__ = '2.4.1'
+__version__ = '2.4.2'
 __author__ = 'Steven Loria'
 
 __all__ = [
