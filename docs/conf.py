29b5ef08e909c879643d7d46b0ab1e43b6265bbb
Steven Loria <sloria1@gmail.com>
2016-05-05 21:44:14 -0400

    Bump version and update changelog

--- docs/conf.py
+++ docs/conf.py
@@ -79,6 +79,7 @@ html_theme_options = {
     'github_user': 'marshmallow-code',
     'github_repo': 'marshmallow',
     'github_banner': True,
+    'github_type': 'star',
     'gratipay_user': 'sloria',
     'code_font_size': '0.8em',
     'warn_bg': '#FFC',
