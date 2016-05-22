fca998d13b78cbc93a98e89df154842caac523e3
Steven Loria <sloria1@gmail.com>
2016-05-22 13:49:40 -0400

    Remove old warning about implicit collection handling

--- marshmallow/schema.py
+++ marshmallow/schema.py
@@ -398,11 +398,6 @@ class BaseSchema(base.SchemaABC):
         .. versionadded:: 1.0.0
         """
         many = self.many if many is None else bool(many)
-        if not many and utils.is_collection(obj) and not utils.is_keyed_tuple(obj):
-            warnings.warn('Implicit collection handling is deprecated. Set '
-                            'many=True to serialize a collection.',
-                            category=DeprecationWarning)
-
         if many and utils.is_iterable_but_not_string(obj):
             obj = list(obj)
 
