1af7a9b69b169bf811e5701bc2427186e3b848fe
Steven Loria <sloria1@gmail.com>
2018-06-26 08:25:30 -0400

    Fix py27 flaking

--- tests/test_py3/test_utils.py
+++ tests/test_py3/test_utils.py
@@ -3,7 +3,7 @@ from marshmallow import fields, Schema
 
 # Regression test for https://github.com/marshmallow-code/marshmallow/issues/540
 def test_function_field_using_type_annotation():
-    def get_split_words(value: str):
+    def get_split_words(value: str):  # flake8: noqa
         return value.split(';')
 
     class MySchema(Schema):
