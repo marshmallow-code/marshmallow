b0ebaf6f13f9833ccc6b19900208b211597480e9
Steven Loria <sloria1@gmail.com>
2018-08-04 08:58:32 -0400

    Bump version and update changelog

--- marshmallow/__init__.py
+++ marshmallow/__init__.py
@@ -14,7 +14,7 @@ from marshmallow.decorators import (
 from marshmallow.utils import pprint, missing
 from marshmallow.exceptions import ValidationError
 
-__version__ = '2.15.3'
+__version__ = '2.15.4'
 __author__ = 'Steven Loria'
 __all__ = [
     'Schema',
