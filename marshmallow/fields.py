2678e728affba63f6783fb6d41ba312f6e76726f
Jared Deckard <jared.deckard@gmail.com>
2018-08-30 09:02:56 -0500

    Don't repeat the nested schema construction code 3 times

--- marshmallow/fields.py
+++ marshmallow/fields.py
@@ -406,34 +406,24 @@ class Nested(Field):
             if isinstance(self.nested, SchemaABC):
                 self.__schema = self.nested
                 self.__schema.context.update(context)
-            elif isinstance(self.nested, type) and \
-                    issubclass(self.nested, SchemaABC):
-                self.__schema = self.nested(
+            else:
+                if isinstance(self.nested, type) and issubclass(self.nested, SchemaABC):
+                    schema_class = self.nested
+                elif not isinstance(self.nested, basestring):
+                    raise ValueError(
+                        'Nested fields must be passed a '
+                        'Schema, not {0}.'.format(self.nested.__class__),
+                    )
+                elif self.nested == 'self':
+                    schema_class = self.parent.__class__
+                else:
+                    schema_class = class_registry.get_class(self.nested)
+                self.__schema = schema_class(
                     many=self.many,
                     only=self.only, exclude=self.exclude, context=context,
                     load_only=self._nested_normalized_option('load_only'),
                     dump_only=self._nested_normalized_option('dump_only'),
                 )
-            elif isinstance(self.nested, basestring):
-                if self.nested == 'self':
-                    parent_class = self.parent.__class__
-                    self.__schema = parent_class(
-                        many=self.many, only=self.only,
-                        exclude=self.exclude, context=context,
-                        load_only=self._nested_normalized_option('load_only'),
-                        dump_only=self._nested_normalized_option('dump_only'),
-                    )
-                else:
-                    schema_class = class_registry.get_class(self.nested)
-                    self.__schema = schema_class(
-                        many=self.many,
-                        only=self.only, exclude=self.exclude, context=context,
-                        load_only=self._nested_normalized_option('load_only'),
-                        dump_only=self._nested_normalized_option('dump_only'),
-                    )
-            else:
-                raise ValueError('Nested fields must be passed a '
-                                 'Schema, not {0}.'.format(self.nested.__class__))
             self.__schema.ordered = getattr(self.parent, 'ordered', False)
         return self.__schema
 
