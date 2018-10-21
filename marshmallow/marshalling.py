b2e5a7da74d2f62a4d3694a43bc31248eb14be24
Jérôme Lafréchoux <jerome@jolimont.fr>
2018-10-21 15:13:25 +0200

    Introduce merge_errors function

--- marshmallow/marshalling.py
+++ marshmallow/marshalling.py
@@ -254,3 +254,53 @@ class Unmarshaller(ErrorStore):
 
     # Make an instance callable
     __call__ = deserialize
+
+
+def merge_errors(errors1, errors2):
+    """Deeply merge two error messages
+
+    Error messages can be string, list of strings or dict of error messages
+    (recursively). Format is the same as accepted by :exc:`ValidationError`.
+    Returns new error messages.
+    """
+    if errors1 is None:
+        return errors2
+    if errors2 is None:
+        return errors1
+    if isinstance(errors1, list):
+        if not errors1:
+            return errors2
+        if isinstance(errors2, list):
+            return errors1 + errors2
+        if isinstance(errors2, dict):
+            return dict(
+                errors2,
+                **{SCHEMA: merge_errors(errors1, errors2.get(SCHEMA))}
+            )
+        return errors1 + [errors2]
+    if isinstance(errors1, dict):
+        if isinstance(errors2, list):
+            return dict(
+                errors1,
+                **{SCHEMA: merge_errors(errors1.get(SCHEMA), errors2)}
+            )
+        if isinstance(errors2, dict):
+            errors = dict(errors1)
+            for key, val in iteritems(errors2):
+                if key in errors:
+                    errors[key] = merge_errors(errors[key], val)
+                else:
+                    errors[key] = val
+            return errors
+        return dict(
+            errors1,
+            **{SCHEMA: merge_errors(errors1.get(SCHEMA), errors2)}
+        )
+    if isinstance(errors2, list):
+        return [errors1] + errors2 if errors2 else errors1
+    if isinstance(errors2, dict):
+        return dict(
+            errors2,
+            **{SCHEMA: merge_errors(errors1, errors2.get(SCHEMA))}
+        )
+    return [errors1, errors2]
--- tests/test_marshalling.py
+++ tests/test_marshalling.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
+
 import pytest
 
 from marshmallow import fields, EXCLUDE, INCLUDE
-from marshmallow.marshalling import Marshaller, Unmarshaller, missing
+from marshmallow.marshalling import Marshaller, Unmarshaller, missing, merge_errors
 from marshmallow.exceptions import ValidationError
 
 from tests.base import User
@@ -254,3 +256,134 @@ class TestUnmarshaller:
         assert 'years' not in result
 
         assert 'always_invalid' not in unmarshal.errors
+
+
+CustomError = namedtuple('CustomError', ['code', 'message'])
+
+
+class TestMergeErrors:
+    def test_merging_none_and_string(self):
+        assert 'error1' == merge_errors(None, 'error1')
+
+    def test_merging_none_and_custom_error(self):
+        assert CustomError(123, 'error1') == merge_errors(None, CustomError(123, 'error1'))
+
+    def test_merging_none_and_list(self):
+        assert ['error1', 'error2'] == merge_errors(None, ['error1', 'error2'])
+
+    def test_merging_none_and_dict(self):
+        assert {'field1': 'error1'} == merge_errors(None, {'field1': 'error1'})
+
+    def test_merging_string_and_none(self):
+        assert 'error1' == merge_errors('error1', None)
+
+    def test_merging_custom_error_and_none(self):
+        assert CustomError(123, 'error1') == merge_errors(CustomError(123, 'error1'), None)
+
+    def test_merging_list_and_none(self):
+        assert ['error1', 'error2'] == merge_errors(['error1', 'error2'], None)
+
+    def test_merging_dict_and_none(self):
+        assert {'field1': 'error1'} == merge_errors({'field1': 'error1'}, None)
+
+    def test_merging_string_and_string(self):
+        assert ['error1', 'error2'] == merge_errors('error1', 'error2')
+
+    def test_merging_custom_error_and_string(self):
+        assert [CustomError(123, 'error1'), 'error2'] == merge_errors(
+            CustomError(123, 'error1'), 'error2',
+        )
+
+    def test_merging_string_and_custom_error(self):
+        assert ['error1', CustomError(123, 'error2')] == merge_errors(
+            'error1', CustomError(123, 'error2'),
+        )
+
+    def test_merging_custom_error_and_custom_error(self):
+        assert [CustomError(123, 'error1'), CustomError(456, 'error2')] == merge_errors(
+            CustomError(123, 'error1'),
+            CustomError(456, 'error2'),
+        )
+
+    def test_merging_string_and_list(self):
+        assert ['error1', 'error2'] == merge_errors('error1', ['error2'])
+
+    def test_merging_string_and_dict(self):
+        assert {'_schema': 'error1', 'field1': 'error2'} == merge_errors(
+            'error1', {'field1': 'error2'},
+        )
+
+    def test_merging_string_and_dict_with_schema_error(self):
+        assert {'_schema': ['error1', 'error2'], 'field1': 'error3'} == merge_errors(
+            'error1', {'_schema': 'error2', 'field1': 'error3'},
+        )
+
+    def test_merging_custom_error_and_list(self):
+        assert [CustomError(123, 'error1'), 'error2'] == merge_errors(
+            CustomError(123, 'error1'), ['error2'],
+        )
+
+    def test_merging_custom_error_and_dict(self):
+        assert {'_schema': CustomError(123, 'error1'), 'field1': 'error2'} == merge_errors(
+            CustomError(123, 'error1'), {'field1': 'error2'},
+        )
+
+    def test_merging_custom_error_and_dict_with_schema_error(self):
+        assert {
+            '_schema': [CustomError(123, 'error1'), 'error2'],
+            'field1': 'error3',
+        } == merge_errors(
+            CustomError(123, 'error1'), {'_schema': 'error2', 'field1': 'error3'},
+        )
+
+    def test_merging_list_and_string(self):
+        assert ['error1', 'error2'] == merge_errors(['error1'], 'error2')
+
+    def test_merging_list_and_custom_error(self):
+        assert ['error1', CustomError(123, 'error2')] == merge_errors(
+            ['error1'], CustomError(123, 'error2'),
+        )
+
+    def test_merging_list_and_list(self):
+        assert ['error1', 'error2'] == merge_errors(['error1'], ['error2'])
+
+    def test_merging_list_and_dict(self):
+        assert {'_schema': ['error1'], 'field1': 'error2'} == merge_errors(
+            ['error1'], {'field1': 'error2'},
+        )
+
+    def test_merging_list_and_dict_with_schema_error(self):
+        assert {'_schema': ['error1', 'error2'], 'field1': 'error3'} == merge_errors(
+            ['error1'], {'_schema': 'error2', 'field1': 'error3'},
+        )
+
+    def test_merging_dict_and_string(self):
+        assert {'_schema': 'error2', 'field1': 'error1'} == merge_errors(
+            {'field1': 'error1'}, 'error2',
+        )
+
+    def test_merging_dict_and_custom_error(self):
+        assert {'_schema': CustomError(123, 'error2'), 'field1': 'error1'} == merge_errors(
+            {'field1': 'error1'}, CustomError(123, 'error2'),
+        )
+
+    def test_merging_dict_and_list(self):
+        assert {'_schema': ['error2'], 'field1': 'error1'} == merge_errors(
+            {'field1': 'error1'}, ['error2'],
+        )
+
+    def test_merging_dict_and_dict(self):
+        assert {
+            'field1': 'error1',
+            'field2': ['error2', 'error3'],
+            'field3': 'error4',
+        } == merge_errors(
+            {'field1': 'error1', 'field2': 'error2'},
+            {'field2': 'error3', 'field3': 'error4'},
+        )
+
+    def test_deep_merging_dicts(self):
+        assert {'field1': {'field2': ['error1', 'error2']}} == merge_errors(
+            {'field1': {'field2': 'error1'}},
+            {'field1': {'field2': 'error2'}},
+        )
