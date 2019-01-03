21e7ea86b26ba08e5ec89ef3994f790c70335b20
Daniel Wallace <danielwallace@gtmanfred.com>
2019-01-03 16:38:36 -0600

    [RFC] optimize get_values for dictionaries
    This method is about twice as fast in my testing on dictionaries, and
    just slightly faster overall.
    
    ```
    root@9f5e81940873:/# bash old.sh; bash old.sh; bash old.sh
    10 loops, best of 3: 1.15 sec per loop
    10 loops, best of 3: 1.13 sec per loop
    10 loops, best of 3: 1.12 sec per loop
    root@9f5e81940873:/# !!:gs/old/new/
    bash new.sh; bash new.sh; bash new.sh
    10 loops, best of 3: 564 msec per loop
    10 loops, best of 3: 571 msec per loop
    10 loops, best of 3: 555 msec per loop
    ```
    
    This is our code dumping 5000 objects comparing the current get_values
    vs the one I am proposing.
    
    And this is the marshmallow bench tests.
    
    ```
    dev $ python performance/benchmark.py; python performance/benchmark.py; python performance/benchmark.py;
    Benchmark Result: 839.86 usec/dump
    Benchmark Result: 851.05 usec/dump
    Benchmark Result: 839.13 usec/dump
    dev $ git checkout gtmanfred-dev dev
    Switched to branch 'gtmanfred-dev'
    Your branch is ahead of 'origin/dev' by 1 commit.
      (use "git push" to publish your local commits)
    gtmanfred-dev $ python performance/benchmark.py; python performance/benchmark.py; python performance/benchmark.py;
    Benchmark Result: 852.94 usec/dump
    Benchmark Result: 811.79 usec/dump
    Benchmark Result: 815.60 usec/dump
    gtmanfred-dev $ python performance/benchmark.py; python performance/benchmark.py; python performance/benchmark.py;
    Benchmark Result: 824.43 usec/dump
    Benchmark Result: 819.98 usec/dump
    Benchmark Result: 856.79 usec/dump
    ```

--- marshmallow/utils.py
+++ marshmallow/utils.py
@@ -335,7 +335,7 @@ def pluck(dictlist, key):
 
 # Various utilities for pulling keyed values from objects
 
-def get_value(obj, key, default=missing):
+def get_value(obj, key, default=missing, delimiter='.'):
     """Helper for pulling a keyed value off various types of objects. Fields use
     this method by default to access attributes of the source object. For object `x`
     and attribute `i`, this method first tries to access `x[i]`, and then falls back to
@@ -346,29 +346,19 @@ def get_value(obj, key, default=missing):
         `get_value` will never check the value `x.i`. Consider overriding
         `marshmallow.fields.Field.get_value` in this case.
     """
-    if not isinstance(key, int) and '.' in key:
-        return _get_value_for_keys(obj, key.split('.'), default)
-    else:
-        return _get_value_for_key(obj, key, default)
-
-
-def _get_value_for_keys(obj, keys, default):
-    if len(keys) == 1:
-        return _get_value_for_key(obj, keys[0], default)
-    else:
-        return _get_value_for_keys(
-            _get_value_for_key(obj, keys[0], default), keys[1:], default,
-        )
-
-
-def _get_value_for_key(obj, key, default):
-    if not hasattr(obj, '__getitem__'):
-        return getattr(obj, key, default)
-
-    try:
-        return obj[key]
-    except (KeyError, IndexError, TypeError, AttributeError):
-        return getattr(obj, key, default)
+    ptr = obj
+    if hasattr(key, '__iter__'):
+        if delimiter in key:
+            for each in key.split(delimiter):
+                ptr = ptr.get(each, getattr(ptr, each, None)) if hasattr(ptr, 'get') else getattr(ptr, each, None)
+                if ptr is None:
+                    return default
+            return ptr
+        else:
+            return ptr.get(key, getattr(ptr, key, default)) if hasattr(ptr, 'get') else getattr(ptr, key, default)
+    elif isinstance(key, int) and key < len(ptr):
+        return ptr[key]
+    return default
 
 
 def set_value(dct, key, value):
