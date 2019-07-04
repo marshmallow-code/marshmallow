d07208f13882db4e683b705d12892fd0325cb6eb
Steven Loria <sloria1@gmail.com>
2019-07-04 13:01:21 -0400

    Officially support Python 3.7

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     project_urls={
