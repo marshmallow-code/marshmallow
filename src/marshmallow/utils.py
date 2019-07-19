7ff7c12e8ede620f74fde0435cdb1d19d2fec089
Steven Loria <sloria1@gmail.com>
2019-07-18 22:15:53 -0400

    Put the common case first

--- src/marshmallow/utils.py
+++ src/marshmallow/utils.py
@@ -346,10 +346,10 @@ def get_func_args(func: typing.Callable) -> typing.List[str]:
     .. versionchanged:: 3.0.0a1
         Do not return bound arguments, eg. ``self``.
     """
-    if isinstance(func, functools.partial):
-        return _signature(func.func)
     if inspect.isfunction(func) or inspect.ismethod(func):
         return _signature(func)
+    if isinstance(func, functools.partial):
+        return _signature(func.func)
     # Callable class
     return _signature(func.__call__)
 
