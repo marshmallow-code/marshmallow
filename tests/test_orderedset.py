6e18806c9b80fabd14b7888d89f52f3c4a1c7313
Juan Norris <juann9@gmail.com>
2020-07-08 15:52:54 -0300

    Parametrize OrderedSet tests

--- tests/test_orderedset.py
+++ tests/test_orderedset.py
@@ -1,22 +1,14 @@
+import pytest
+
 from marshmallow import orderedset
 
 
-def test_intersection():
+@pytest.mark.parametrize(
+    ("method_name", "result"),
+    [("intersection", {2, 3}), ("union", {1, 2, 3}), ("difference", {1})],
+)
+def test_methods(method_name, result):
     s = orderedset.OrderedSet([2, 1, 3])
     t = orderedset.OrderedSet([3, 2])
 
-    assert s.intersection(t) == {2, 3}
-
-
-def test_union():
-    s = orderedset.OrderedSet([2, 1, 3])
-    t = orderedset.OrderedSet([3, 2])
-
-    assert s.union(t) == {1, 2, 3}
-
-
-def test_difference():
-    s = orderedset.OrderedSet([2, 1, 3])
-    t = orderedset.OrderedSet([3, 2])
-
-    assert s.difference(t) == {1}
+    assert getattr(s, method_name)(t) == result
