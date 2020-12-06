24ea6f0f646b6a042fb2575753f9c75be2cdb249
Steven Loria <sloria1@gmail.com>
2020-12-05 21:50:37 -0500

    Upgrade autodocsumm to fix #1703

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ EXTRAS_REQUIRE = {
         "sphinx-issues==1.2.0",
         "alabaster==0.7.12",
         "sphinx-version-warning==1.1.2",
-        "autodocsumm==0.2.1",
+        "autodocsumm==0.2.2",
     ],
 }
 EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["lint"] + ["tox"]
