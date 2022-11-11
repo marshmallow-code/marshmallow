083cc16e4d1e1141b9610faa393cd61710de62b7
Jérôme Lafréchoux <lafrech@users.noreply.github.com>
2022-11-11 17:12:35 +0100

    Support Python 3.11 (#2067)
    * Support Python 3.11
    
    * Update changelog
    
    Co-authored-by: Steven Loria <sloria1@gmail.com>

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     test_suite="tests",
     project_urls={
