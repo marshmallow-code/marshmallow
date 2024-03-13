3e26fa20104580d6724746300fff5edbf82aacfe
Steven Loria <sloria1@gmail.com>
2024-03-13 14:52:30 -0400

    chore: format docstrings with ruff (#2248)

--- src/marshmallow/validate.py
+++ src/marshmallow/validate.py
@@ -47,9 +47,11 @@ class And(Validator):
 
         from marshmallow import validate, ValidationError
 
+
         def is_even(value):
             if value % 2 != 0:
                 raise ValidationError("Not an even value.")
+
 
         validator = validate.And(validate.Range(min=0), is_even)
         validator(-1)
