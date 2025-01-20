4fc1e37c6ba0f1d350b902e4275f1d51aebecb90
Steven Loria <sloria1@gmail.com>
2025-01-20 18:57:54 -0500

    formatting

--- src/marshmallow/fields.py
+++ src/marshmallow/fields.py
@@ -187,12 +187,12 @@ class Field(typing.Generic[_InternalT]):
         data_key: str | None = None,
         attribute: str | None = None,
         validate: types.Validator | typing.Iterable[types.Validator] | None = None,
-        pre_load: types.PreLoadCallable
-        | typing.Iterable[types.PreLoadCallable]
-        | None = None,
-        post_load: types.PostLoadCallable
-        | typing.Iterable[types.PostLoadCallable]
-        | None = None,
+        pre_load: (
+            types.PreLoadCallable | typing.Iterable[types.PreLoadCallable] | None
+        ) = None,
+        post_load: (
+            types.PostLoadCallable | typing.Iterable[types.PostLoadCallable] | None
+        ) = None,
         required: bool = False,
         allow_none: bool | None = None,
         load_only: bool = False,
